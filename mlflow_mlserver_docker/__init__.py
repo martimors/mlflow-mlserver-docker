50c52f54e6e5c399e424b3c93fff1db55797a384
Martin Morset <mmo@one.com>
2022-09-20 16:31:55 +0200

    fix: use latest poetry version in publish

--- mlflow_mlserver_docker/__init__.py
+++ mlflow_mlserver_docker/__init__.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 # Keep this in sync with pyproject.toml manually
 # Or use this https://github.com/tiangolo/poetry-version-plugin
 # after this is resolved: https://github.com/tiangolo/poetry-version-plugin/issues/27
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 load_dotenv()
